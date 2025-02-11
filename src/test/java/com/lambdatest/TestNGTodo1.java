3d1b7eaff17e66b67a7f21c17d146f9868ba7736
tanvin11 <T@lha1020>
2025-02-11 18:09:14 -0500

    updating driver code

--- src/test/java/com/lambdatest/TestNGTodo1.java
+++ src/test/java/com/lambdatest/TestNGTodo1.java
@@ -13,6 +13,9 @@ import org.testng.ITestContext;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.openqa.selenium.WebDriver;
 
 public class TestNGTodo1 {
