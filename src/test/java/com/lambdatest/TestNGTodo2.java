23404cf453ab37b8c5f2aa7fbe7de36bb5e7ff44
tanvin11 <T@lha1020>
2025-02-11 17:43:32 -0500

    pom.xml change1

--- src/test/java/com/lambdatest/TestNGTodo2.java
+++ src/test/java/com/lambdatest/TestNGTodo2.java
@@ -14,6 +14,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
 
 
 
